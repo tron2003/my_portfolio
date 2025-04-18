76e243107be434bcd79fd0defdef7310a0a1ca47
Kanish Mahato <kanishm245@gmail.com.com>
2025-04-19 01:35:42 +0530

    till correct done hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
 import { styles } from "../style";
-import { ComputersCanvas } from "./canvas"
+import { ComputersCanvas } from "./canvas";
+// import { repeat } from "maath/dist/declarations/src/misc";
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto ">
       <div
-        className={`${styles.paddingX} absolute inset-0  top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`absolute  inset-0  top-[120px] max-w-7xl mx-auto ${styles.paddingX}   flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-4 h-4 rounded-full bg-[#915eff]" />
@@ -13,16 +14,31 @@ const Hero = () => {
         </div>
         <div className={`${styles.heroHeadText} text-white`}>
           <h1>
-            Hi, I'm <span className="text-[#915eff]">Kanish</span>
+            Hi, I'm <span className="text-[#915EFF]">Kanish</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
+          <p className={`${styles.heroSubText} mt-2 text-gray-100 `}>
             I develop intelligent AI models, user-friendly
             <br className="sm:block hidden" /> interfaces, and full-stack web
             applications
           </p>
         </div>
       </div>
-        <ComputersCanvas/>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { Html, useProgress } from "@react-three/drei";
 const Loader = () => {
   const { progress } = useProgress();
   return (
-    <Html>
+    <Html >
       <span className="canvas-loader"></span>
       <p
         style={{
