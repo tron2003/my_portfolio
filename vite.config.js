0079fdbd712321f5fc892441ace7a3d2433aca1f
Kanish Mahato <kanishm245@gmail.com.com>
2025-04-20 21:08:04 +0530

    change in vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['my-portfolio-4y0r.onrender.com']
+  }
 })
