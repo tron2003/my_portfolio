f55f35e93a65e8f4b7c749e86ef2e000d4a7fa92
Kanish Mahato <kanishm245@gmail.com.com>
2025-04-21 12:27:32 +0530

    capital issue

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import portfolio from "./portfolio.png"
+import portfolio from "./Portfolio.png"
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
